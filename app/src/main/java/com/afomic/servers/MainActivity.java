0ef135b97fee7f452d6794119ab68b0e0b6fd8af
afomic <afomic1@gmail.com>
2017-09-16 13:10:47 +0100

    i created waiter main activiity

--- app/src/main/java/com/afomic/servers/MainActivity.java
+++ app/src/main/java/com/afomic/servers/MainActivity.java
@@ -1,7 +1,12 @@
 package com.afomic.servers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.afomic.servers.kitchen.activities.KitchenActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button kitchenLoginButton=(Button) findViewById(R.id.btn_kitchen);
+        kitchenLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent=new Intent(MainActivity.this, KitchenActivity.class);
+                startActivity(mIntent);
+            }
+        });
+        Button waiterLoginButton=(Button) findViewById(R.id.btn_waiter);
+
+        waiterLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/afomic/servers/waiters/activities/WaiterActivity.java
@@ -0,0 +1,15 @@
+package com.afomic.servers.waiters.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.afomic.servers.R;
+
+public class WaiterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiter);
+    }
+}
