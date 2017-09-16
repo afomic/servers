03ad80a5c764b84f276562198b5cc544281897d0
afomic <afomic1@gmail.com>
2017-09-16 05:52:26 +0100

    I created Table object

--- app/src/main/java/com/afomic/servers/model/Table.java
+++ app/src/main/java/com/afomic/servers/model/Table.java
@@ -1,8 +1,33 @@
 package com.afomic.servers.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by rechael on 9/15/2017.
+ * this is the model class for Table
  */
 
 public class Table {
+    public static final int NEW=0;
+    public static final int ORDER_TAKEN=1;
+    public static final int SERVED=1;
+    private ArrayList<Order> mTableOrders;
+    private String mName;
+    private String mWaiterName;
+    private int mStatus;
+
+    public Table(String name,ArrayList<Order> tableOrders,String waiterName,int status){
+        mName=name;
+        mTableOrders=tableOrders;
+        mWaiterName=waiterName;
+        mStatus=status;
+
+    }
+    public Table(String name){
+        mStatus=NEW;
+        mTableOrders=new ArrayList<>();
+        mName=name;
+        mWaiterName="";
+    }
+
 }
